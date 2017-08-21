6343e372a09a3663ee0a7b62e222ced32a0c03c8
chetankolte <chetankolte56@gmail.com>
2017-08-21 21:54:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,18 +1,3 @@
-console.log('Loaded!');
-//Change the text
-var element = document.getElementById('main-text');
-element.innerHTML = 'New Value';
-//Move the image
-var img = document.getElementById('madi');
-var marginLeft = 0;
-function moveRight() {
-    marginLeft = marginLeft + 2;
-    img.style.marginLeft = marginLeft + "px";
-}
-img.onclick = function(){
-    var interval = setInterval(moveRight, 50);
-};
-
 //counter
 var button = document.getElementById("counter");
 var counter=0;
