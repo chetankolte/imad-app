313fa3dc3dad457a23c5fd8cda225d401b13aa04
chetankolte <chetankolte56@gmail.com>
2017-08-23 16:40:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,13 +2,22 @@
 var button = document.getElementById('counter');
 var counter=0;
 button.onclick = function(){
-    
-    //make a request to the counter endpoint
-    
+    //Create a request object
+    var request = new XMLHttpRequest();
+ 
     //Capture a response and store it in a variable
-    
-    //Render the variable in the correct span
-    counter = counter+1;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
+    request.onreadystatechange = function(){
+        if(request.readyState === XMLHttpRequest.DONE){
+            //Take some action
+        if (request.status === 200){
+            var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            
+        }
+    }
+    };
+    //make the request
+    request.open('GET', 'http://chetankolte56.imad.hasura-app.io/counter', true);
+    request.send(null);
 };
