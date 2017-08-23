3f171b77c1ee4f12b44e9138d3566af8b3137989
chetankolte <chetankolte56@gmail.com>
2017-08-23 22:49:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -71,7 +71,7 @@ app.get('/counter', function(req, res){
 });
 
 var names = [];
-app.get('/submit-name', function(req, res){
+app.get('/submit-name', function(req, res){ //URL: /submit-name?name=xxxx
     //Get the name from the request
     var name = req.query.name; //TODO
 
